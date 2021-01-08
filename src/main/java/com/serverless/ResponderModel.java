fba2e66d0623d2721888c063591482f4191d6ae7
brianhsb1671 <brianhsb1671@gmail.com>
2021-01-08 09:24:45 -0500

    what is your name

--- src/main/java/com/serverless/ResponderModel.java
+++ src/main/java/com/serverless/ResponderModel.java
@@ -4,19 +4,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ResponderModel {
-    private final String teamName = "devco";
+    private final String teamName = "LosMilagrosos";
 
     public String answer(String question) {
+
         if ("".equals(question)){
             return teamName;
         }
 
-        Matcher sumMatcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(question);
-        if (sumMatcher.matches()) {
-            return String.valueOf(Integer.parseInt(sumMatcher.group(1)) + Integer.parseInt(sumMatcher.group(2)));
+        Matcher matcher = Pattern.compile(".*what is your name").matcher(question);
+        if (matcher.matches()) {
+            return teamName;
+        }
+
+        matcher = Pattern.compile(".*what is the sum of (\\d+) and (\\d+)").matcher(question);
+        if (matcher.matches()) {
+            return String.valueOf(Integer.parseInt(matcher.group(1)) + Integer.parseInt(matcher.group(2)));
         }
         
         return teamName;
     }
-
 }
