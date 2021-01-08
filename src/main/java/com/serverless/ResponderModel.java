47cb141ea0713cfda00ac9e14cdafc3d9b4f722e
brianhsb1671 <brianhsb1671@gmail.com>
2021-01-08 09:50:21 -0500

    greater than

--- src/main/java/com/serverless/ResponderModel.java
+++ src/main/java/com/serverless/ResponderModel.java
@@ -21,6 +21,14 @@ public class ResponderModel {
         if (matcher.matches()) {
             return String.valueOf(Integer.parseInt(matcher.group(1)) + Integer.parseInt(matcher.group(2)));
         }
+
+        matcher = Pattern.compile(".*which of the following numbers is the largest: (\\d+), (\\d+)").matcher(question);
+        if (matcher.matches()) {
+            if(Integer.parseInt(matcher.group(1)) >= Integer.parseInt(matcher.group(2))){
+                return matcher.group(1);
+            }
+            return matcher.group(2);
+        }
         
         return teamName;
     }
